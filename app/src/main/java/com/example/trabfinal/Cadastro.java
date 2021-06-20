ba7aaaec2ecb7c2d4c1ec362633fca7fa8304049
Camila Minei <camisminei.ufms@gmail.com>
2021-06-20 10:29:40 -0400

    cadastro xml e cadastro java

---
+++ app/src/main/java/com/example/trabfinal/Cadastro.java
@@ -0,0 +1,11 @@
+package com.example.trabfinal;
+
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Cadastro extends AppCompatActivity {
+    private EditText edtEmail, edtTelefone, edtSenha; 
+
+
+}
