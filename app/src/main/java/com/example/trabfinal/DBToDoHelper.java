df2ddb1c910112ff5d3e6bc3929a612259a0a673
m4phx <50882395+m4phx@users.noreply.github.com>
2021-06-25 07:43:32 -0400

    Add sql pra onCreate e onUpgrade

--- app/src/main/java/com/example/trabfinal/DBToDoHelper.java
+++ app/src/main/java/com/example/trabfinal/DBToDoHelper.java
@@ -59,12 +59,16 @@ public class DBToDoHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(TABLE_CREATE_TAREFA);
+        db.execSQL(TABLE_CREATE_USUARIO);
         this.db = db;
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         String query = "DROP TABLE IF EXISTS " + TABLE_NAME_TAREFA + ";";
+        db.execSQL(query);
+        query = "DROP TABLE IF EXISTS " + TABLE_NAME_USUARIO + ";";
+        db.execSQL(query);
     }
 
     public long insert_Tarefa(Tarefas t) {
